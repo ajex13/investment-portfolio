6914f9279cbeeeea7c46cb51ddd28e505abdc7c1
Ajinkya Devasthali <ajinkya.devasthali@lohono.com>
2025-08-22 03:30:13 +0530

    react initial config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/stocks").then((res) => {
+      setStocks(res.data);
+    });
+
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>Investment Portfolio</h2>
+      <ul>
+        {stocks.map((s) => (
+          <li key={s._id}>
+            {s.symbol}: INR{s.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
